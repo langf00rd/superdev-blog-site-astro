a06c6f2a9ec4bc069a54d66d87bc347e05c98173
langford <langfordquarshie21@gmail.com>
2022-08-24 16:01:31 +0000

    add: videos

---
+++ src/lib/videos.js
@@ -0,0 +1,33 @@
+export const videos = [
+  {
+    title: "How to Create a Blog Card | HTML CSS tutorial",
+    date: "Aug 20, 2022",
+    link: "https://www.youtube.com/watch?v=bOLqf2mrzpo",
+    thumbnail:
+      "https://i9.ytimg.com/vi/bOLqf2mrzpo/mqdefault.jpg?v=63016884&sqp=CMzniZgG&rs=AOn4CLA74WzawQMXAaCEotPi0xhcLwINHg",
+  },
+
+  {
+    title: "How to Create a Custom Mouse Pointer | HTML CSS JavaScript",
+    date: "Aug 17, 2022",
+    link: "https://www.youtube.com/watch?v=1SOTC3u99JI",
+    thumbnail:
+      "https://i9.ytimg.com/vi_webp/1SOTC3u99JI/mqdefault.webp?v=62fcc113&sqp=CPjpiZgG&rs=AOn4CLAZwjOFubM0rXvbO5iST4cylTuLKg",
+  },
+
+  {
+    title: "Wrapping text around image HTML CSS Tutorial",
+    date: "Feb 3, 2020",
+    link: "https://www.youtube.com/watch?v=5Uz33vo4GoQ",
+    thumbnail:
+      "https://i9.ytimg.com/vi_webp/5Uz33vo4GoQ/mqdefault.webp?v=5e3783b7&sqp=CPjpiZgG&rs=AOn4CLAXUWwLhrirtGSZgRISv6njPj_L8w",
+  },
+
+  {
+    title: "Creating a Header/ Nav bar for a website HTML and CSS",
+    date: "Jan 19, 2020",
+    link: "https://www.youtube.com/watch?v=Az3dqHD9omY",
+    thumbnail:
+      "https://i9.ytimg.com/vi_webp/Az3dqHD9omY/mqdefault.webp?v=5e2c2757&sqp=CKTsiZgG&rs=AOn4CLCciQUT23_KzoPXQkHX5dAueMpVqg",
+  },
+];
